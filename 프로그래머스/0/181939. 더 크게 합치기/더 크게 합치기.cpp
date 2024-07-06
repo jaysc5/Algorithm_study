9fe0fb883f96ea42fb3faf394aa67436ae368228
jaysc5 <113988349+jaysc5@users.noreply.github.com>
2024-07-05 20:36:46 -0700

    [level 0] Title: 더 크게 합치기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.cpp
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.cpp
@@ -4,14 +4,5 @@
 using namespace std;
 
 int solution(int a, int b) {
-    string sumab = to_string(a) + to_string(b);
-    string sumba = to_string(b) + to_string(a);
-    
-    int sumAB = stoi(sumab);
-    int sumBA = stoi(sumba);
-    
-    if (sumAB >= sumBA)
-        return sumAB;
-    else
-        return sumBA;
+    return max(stoi(to_string(a) + to_string(b)), stoi(to_string(b) + to_string(a)));
 }
