c37be252563959e9f6da7c1ea79c219bf1f30bb9
jaysc5 <113988349+jaysc5@users.noreply.github.com>
2024-07-06 22:51:27 -0700

    [level 0] Title: 코드 처리하기, Time: 0.01 ms, Memory: 3.69 MB -BaekjoonHub

---
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.cpp
@@ -0,0 +1,28 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string code) {
+    string ret = "";
+    int mode = 0;
+    
+    for(int idx=0; idx<code.size(); idx++){
+        if (mode == 0){
+            if (code[idx] != '1'){
+                if (idx%2==0)
+                    ret += code[idx];
+            }
+            else mode = 1;
+        }
+        else if (mode == 1) {
+            if (code[idx] != '1'){
+                if (idx%2!=0)
+                    ret += code[idx];
+            }
+            else mode = 0;
+        }
+    }
+    if(ret.empty()) return "EMPTY";
+    else return ret;
+}
