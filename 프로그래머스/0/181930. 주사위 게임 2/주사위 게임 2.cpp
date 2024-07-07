9e5e8079a3098bcbafa585a6e3e933d1ceb40f67
jaysc5 <113988349+jaysc5@users.noreply.github.com>
2024-07-06 23:01:07 -0700

    [level 0] Title: 주사위 게임 2, Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

---
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+int solution(int a, int b, int c) {
+    int answer = 0;
+    
+    if (a!=b && a!=c && b!=c) answer = a+b+c;
+    else if ((a==b && a!=c) || (a==c && a!=b) || (b==c && a!=c)) answer = ((a+b+c) * (pow(a,2)+pow(b,2)+pow(c,2)));
+    else if (a==b && a==c) answer = ((a+b+c) * (pow(a,2)+pow(b,2)+pow(c,2)) * (pow(a,3)+pow(b,3)+pow(c,3)));
+    return answer;
+}
