d5759b7b1af5cbaef0d4a0aa0e9c4404114c6d5c
jaysc5 <113988349+jaysc5@users.noreply.github.com>
2024-07-08 09:23:18 +0000

    [level 0] Title: 배열 만들기 4, Time: 0.14 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.cpp
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.cpp
@@ -1,24 +1,15 @@
 #include <string>
 #include <vector>
-
+#include <algorithm>
 using namespace std;
 
 vector<int> solution(vector<int> arr) {
     vector<int> stk;
     int i = 0;
-    
-    while(i < arr.size()) {
-        if(stk.empty()) {
-            stk.push_back(arr[i]);
-            i++;
-        }
-        else if(stk.empty() == 0 && stk[stk.size() - 1] < arr[i]) {
-            stk.push_back(arr[i]);
-            i++;
-        }
-        else if(stk.empty() == 0 && stk[stk.size() - 1] >= arr[i]) {
-            stk.pop_back();
-        }
+    while (i<arr.size()) {
+        if (stk.empty()) stk.push_back(arr[i++]);
+        else if (stk.back() < arr[i]) stk.push_back(arr[i++]);
+        else stk.erase(stk.end()-1);
     }
     return stk;
 }
