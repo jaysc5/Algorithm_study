e488aec2ef040a4e7d0a0e5b8494f1b1a473fa8c
jaysc5 <113988349+jaysc5@users.noreply.github.com>
2024-07-13 09:55:16 +0000

    [level 0] Title: 원하는 문자열 찾기, Time: 0.29 ms, Memory: 3.87 MB -BaekjoonHub

--- 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.cpp
+++ 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.cpp
@@ -8,8 +8,6 @@ int solution(string myString, string pat) {
     transform(myString.begin(), myString.end(), myString.begin(), ::tolower);
     transform(pat.begin(), pat.end(), pat.begin(), ::tolower);
     
-    if(myString.size() >= pat.size()) {
-        if(myString.find(pat) != string::npos) return 1;
-    }
-    return 0;
+    if(myString.size() >= pat.size())
+        return myString.find(pat) != string::npos;
 }
