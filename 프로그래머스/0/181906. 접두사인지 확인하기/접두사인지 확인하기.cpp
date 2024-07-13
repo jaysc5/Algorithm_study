ccba5f07f553176a6b1f2a31d9dea0af14918c51
jaysc5 <113988349+jaysc5@users.noreply.github.com>
2024-07-13 09:41:29 +0000

    [level 0] Title: 접두사인지 확인하기, Time: 0.01 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.cpp
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.cpp
@@ -5,10 +5,5 @@
 using namespace std;
 
 int solution(string my_string, string is_prefix) {
-    vector<string> v;
-    for(int i=1; i<my_string.size(); i++)
-        v.push_back(my_string.substr(0, i));
-    if (find(v.begin(), v.end(), is_prefix) == v.end())
-        return 0;
-    return 1;
+    return my_string.substr(0,is_prefix.size()) == is_prefix;
 }
