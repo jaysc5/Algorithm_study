db785a7ee7b0d8a246d82ce3706a60273d5ea39d
jaysc5 <113988349+jaysc5@users.noreply.github.com>
2024-07-17 13:15:40 +0000

    [level 0] Title: 리스트 자르기, Time: 0.03 ms, Memory: 3.69 MB -BaekjoonHub

--- 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.cpp
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.cpp
@@ -4,23 +4,18 @@
 using namespace std;
 
 vector<int> solution(int n, vector<int> slicer, vector<int> num_list) {
-    vector<int> answer;
     int a = slicer[0];
     int b = slicer[1];
     int c = slicer[2];
     
     switch (n){
-        case 1:
-            answer = vector<int>(num_list.begin(), num_list.begin()+b+1);
-            break;
-        case 2: answer = vector<int>(num_list.begin()+a, num_list.end());
-            break;
-        case 3: answer = vector<int>(num_list.begin()+a, num_list.begin()+b+1);
-            break;
+        case 1: return vector<int>(num_list.begin(), num_list.begin()+b+1);
+        case 2: return vector<int>(num_list.begin()+a, num_list.end());
+        case 3: return vector<int>(num_list.begin()+a, num_list.begin()+b+1);
         case 4:
+            vector<int> answer;
             for(int i=a; i<=b; i+=c)
                 answer.push_back(num_list[i]);
-            break;    
+            return answer;
     }
-    return answer;
 }
