5e71f7c35e8de684c24ed32539bf8917053767c5
jaysc5 <113988349+jaysc5@users.noreply.github.com>
2024-07-17 06:48:51 +0000

    [level 0] Title: 배열 만들기 1, Time: 1.29 ms, Memory: 4.34 MB -BaekjoonHub

---
+++ 프로그래머스/0/181901. 배열 만들기 1/배열 만들기 1.cpp
@@ -0,0 +1,11 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(int n, int k) {
+    vector<int> answer;
+    for (int i=1; i<=n/k; i++)
+        answer.push_back(i*k);
+    return answer;
+}
