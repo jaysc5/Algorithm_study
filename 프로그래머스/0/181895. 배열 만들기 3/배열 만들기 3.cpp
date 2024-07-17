b260f76693d4686e36abcb7cc97ee27eac15af8d
jaysc5 <113988349+jaysc5@users.noreply.github.com>
2024-07-17 13:24:48 +0000

    [level 0] Title: 배열 만들기 3, Time: 20.02 ms, Memory: 19.8 MB -BaekjoonHub

--- 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.cpp
+++ 프로그래머스/0/181895. 배열 만들기 3/배열 만들기 3.cpp
@@ -4,8 +4,8 @@
 using namespace std;
 
 vector<int> solution(vector<int> arr, vector<vector<int>> intervals) {
-    vector<int> v0 = vector<int>(arr.begin()+intervals[0][0], arr.begin()+intervals[0][1]+1);
-    vector<int> v1 = vector<int>(arr.begin()+intervals[1][0], arr.begin()+intervals[1][1]+1);
-    v0.insert(v0.end(), v1.begin(), v1.end());
-    return v0;
+    vector<int> answer;
+    for (auto itv : intervals)
+        answer.insert(answer.end(), arr.begin()+itv[0], arr.begin()+itv[1]+1);
+    return answer;
 }
