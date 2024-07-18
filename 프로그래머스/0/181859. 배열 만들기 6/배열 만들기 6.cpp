c52c619f7535841326823a7c13cacea4b995493b
jaysc5 <113988349+jaysc5@users.noreply.github.com>
2024-07-18 14:37:42 +0000

    [level 0] Title: 배열 만들기 6, Time: 7.72 ms, Memory: 32.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr) {
+    vector<int> stk;
+    for (int i=0; i<arr.size(); i++){
+        if (stk.empty()) stk.push_back(arr[i]);
+        else if (!stk.empty() & stk.back()==arr[i]) stk.erase(stk.end()-1);
+        else if (!stk.empty() & stk.back()!=arr[i]) stk.push_back(arr[i]);
+    }
+    if (stk.empty()) return {-1};
+    else return stk;
+}
