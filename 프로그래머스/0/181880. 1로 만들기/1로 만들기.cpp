c88eb2919f071188baa6b6c1f2c574a5eaca5edf
jaysc5 <113988349+jaysc5@users.noreply.github.com>
2024-07-18 13:37:05 +0000

    [level 0] Title: 1로 만들기, Time: 0.01 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/0/181880. 1로 만들기/1로 만들기.cpp
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.cpp
@@ -8,8 +8,7 @@ int solution(vector<int> num_list) {
     int answer = 0;
     for (int &num : num_list){
         while (num!=1){
-            if (num%2==0) num /= 2;
-            else num = (num-1) / 2;
+            num /= 2;
             answer++;
         }
     }
