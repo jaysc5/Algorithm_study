94bee28365412a53e0a92ebc54de280b2915c042
jaysc5 <113988349+jaysc5@users.noreply.github.com>
2024-07-18 16:54:06 +0000

    [level 0] Title: 0 떼기, Time: 0.02 ms, Memory: 4.01 MB -BaekjoonHub

--- 프로그래머스/0/181847. 0 떼기/0 떼기.cpp
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.cpp
@@ -4,11 +4,5 @@
 using namespace std;
 
 string solution(string n_str) {
-    string answer = "";
-    int idx = 0;
-    for (char s : n_str) {
-        if (s=='0') idx+=1;
-        else break;
-    }
-    return n_str.substr(idx);
+    return to_string(stoi(n_str));
 }
