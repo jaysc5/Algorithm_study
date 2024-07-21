89f2f767464bcd8c0fb8c7cff7658f03231e9126
jaysc5 <113988349+jaysc5@users.noreply.github.com>
2024-07-21 07:17:25 +0000

    [level 0] Title: 주사위 게임 1, Time: 0.01 ms, Memory: 4.01 MB -BaekjoonHub

--- 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.cpp
+++ 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.cpp
@@ -1,11 +1,10 @@
 #include <string>
 #include <vector>
-#include <cmath>
 
 using namespace std;
 
 int solution(int a, int b) {
-    if (a%2!=0 & b%2!=0) return pow(a,2) + pow(b,2);
+    if (a%2!=0 & b%2!=0) return a*a+b*b;
     else if (a%2==0 & b%2==0) return abs(a-b);
     else return 2*(a+b);
 }
