daca5db0d2a048205cc40ca2f46175054a1f400e
jaysc5 <113988349+jaysc5@users.noreply.github.com>
2024-07-21 06:58:50 +0000

    [level 0] Title: 부분 문자열인지 확인하기, Time: 0.02 ms, Memory: 3.72 MB -BaekjoonHub

--- 프로그래머스/0/181843. 부분 문자열인지 확인하기/부분 문자열인지 확인하기.cpp
+++ 프로그래머스/0/181843. 부분 문자열인지 확인하기/부분 문자열인지 확인하기.cpp
@@ -4,15 +4,5 @@
 using namespace std;
 
 int solution(string my_string, string target) {
-    if (my_string.size()<target.size()) return 0;
-    else if (my_string.size() == target.size()) {
-        if (my_string == target) return 1;
-    }
-    else {
-        for(int i=0; i<=my_string.size()-target.size(); i++){
-            if (my_string.substr(i,target.size()) == target)
-                return 1;
-        }
-    }
-    return 0;
+    return (my_string.find(target) != string::npos);
 }
