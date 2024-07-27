d3661e9c3ce241e965eefd56e74c22a402d683f2
jaysc5 <113988349+jaysc5@users.noreply.github.com>
2024-07-27 08:04:34 +0000

    [level 1] Title: 약수의 합, Time: 0.04 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.py
@@ -0,0 +1,13 @@
+def solution(n):
+
+    divisorsList = []
+
+    for i in range(1, int(n**(1/2)) + 1):
+        if (n % i == 0):
+            divisorsList.append(i) 
+            if ( (i**2) != n) : 
+                divisorsList.append(n // i)
+
+    divisorsList.sort()
+    
+    return sum(divisorsList)
