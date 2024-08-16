3d7892fb8acbc26c37fa0bc21af25411e48ba62e
jaysc5 <113988349+jaysc5@users.noreply.github.com>
2024-08-16 11:46:42 +0000

    [level 1] Title: 카드 뭉치, Time: 0.01 ms, Memory: 3.68 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.cpp
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.cpp
@@ -4,11 +4,13 @@
 using namespace std;
 
 string solution(vector<string> cards1, vector<string> cards2, vector<string> goal) {
+    int idx1 = 0 , idx2 = 0;
+    
     for (string g : goal){
-        if (!cards1.empty() and g == cards1[0])
-            cards1.erase(cards1.begin());
-        else if (!cards2.empty() and g == cards2[0])
-            cards2.erase(cards2.begin());
+        if (!cards1.empty() and g == cards1[idx1])
+            idx1++;
+        else if (!cards2.empty() and g == cards2[idx2])
+            idx2++;
         else
             return "No";
     }
