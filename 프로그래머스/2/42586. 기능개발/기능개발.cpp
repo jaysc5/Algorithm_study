d3cf07626807772e577ae4b920df5706fb865201
jaysc5 <113988349+jaysc5@users.noreply.github.com>
2024-08-16 11:32:10 +0000

    [level 2] Title: 기능개발, Time: 0.14 ms, Memory: 3.63 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.cpp
+++ 프로그래머스/2/42586. 기능개발/기능개발.cpp
@@ -1,29 +1,26 @@
 #include <string>
 #include <vector>
 #include <iostream>
-#include <cmath>
 using namespace std;
 
 vector<int> solution(vector<int> progresses, vector<int> speeds) {
     vector<int> answer;
-    vector<int> q;
 
-    for (int i=0; i<progresses.size(); i++){
-        q.push_back(ceil((100.0-progresses[i])/speeds[i]));
-    }
-    
-    int remain = q[0];
-    int count = 0;
+    int day;
+    int max_day = 0;
+    for (int i = 0; i < progresses.size(); ++i)
+    {
+        day = (99 - progresses[i]) / speeds[i] + 1;
+        cout << day << endl;
+        
+        if (answer.empty() || max_day < day)
+            answer.push_back(1);
+        else
+            ++answer.back();
 
-    for (int i=0; i<progresses.size(); i++){
-        if (q[i] <= remain)
-            count++;
-        else{
-            answer.push_back(count);
-            remain = q[i];
-            count = 1;
-        }
+        if (max_day < day)
+            max_day = day;
     }
-    answer.push_back(count);
+
     return answer;
 }
