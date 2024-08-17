e1c5142d135b93c58d93d0bf19b9ed7b45beeeed
jaysc5 <113988349+jaysc5@users.noreply.github.com>
2024-08-17 03:48:59 +0000

    [level 2] Title: 영어 끝말잇기, Time: 0.04 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+vector<int> solution(int n, vector<string> words) {
+    vector<int> answer;
+    unordered_map<string, string> wordgame;
+    
+    for (int i=0; i<words.size()-1; i++){
+        if (wordgame.find(words[i]) == wordgame.end()){
+            if (words[i][words[i].size()-1]==words[i+1][0])
+                wordgame[words[i]] = words[i+1];
+            else
+                return vector<int>{(i+1)%n+1, (i+1)/n+1};
+        }
+        else
+            return vector<int>{i%n+1, i/n+1};
+        
+    }
+    int k = words.size()-1;
+    if (wordgame.find(words[k])== wordgame.end())
+        return vector<int>{0, 0};
+    else 
+        return vector<int>{k%n+1, k/n+1};
+}
