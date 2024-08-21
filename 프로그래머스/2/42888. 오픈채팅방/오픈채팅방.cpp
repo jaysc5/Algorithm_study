8d4940a7ac240f86d06ad54bd71b04aa510656a0
jaysc5 <113988349+jaysc5@users.noreply.github.com>
2024-08-21 21:15:18 +0000

    [level 2] Title: 오픈채팅방, Time: 208.10 ms, Memory: 43.7 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.cpp
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.cpp
@@ -1,9 +1,36 @@
 #include <string>
 #include <vector>
+#include <unordered_map>
+#include <sstream>
 
 using namespace std;
 
 vector<string> solution(vector<string> record) {
     vector<string> answer;
+    unordered_map<string, string> users;
+    
+    for (int i=0; i<record.size(); i++){
+        stringstream ss(record[i]);
+        string state, uid, name;
+        ss >> state >> uid;
+
+        if (state == "Enter" or state == "Change"){
+            ss >> name;
+            users[uid] = name;
+        }
+    }
+    
+    for (int i=0; i<record.size(); i++){
+        stringstream ss(record[i]);
+        string state, uid;
+        ss >> state >> uid;
+
+        if (state == "Enter"){
+            answer.push_back(users[uid] + "님이 들어왔습니다.");
+        }
+        else if (state == "Leave"){
+            answer.push_back(users[uid] + "님이 나갔습니다.");
+        }
+    }
     return answer;
 }
