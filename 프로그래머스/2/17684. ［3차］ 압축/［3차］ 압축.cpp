b73cf1ccbfbd23cdc56e39fee7fb7244103a0217
jaysc5 <113988349+jaysc5@users.noreply.github.com>
2024-08-26 14:22:21 +0000

    [level 2] Title: [3차] 압축, Time: 0.55 ms, Memory: 3.86 MB -BaekjoonHub

---
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.cpp
@@ -0,0 +1,35 @@
+#include <string>
+#include <vector>
+#include <map>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(string msg) {
+    vector<int> answer;
+    map<string,int> m;
+    string s = "";
+   
+    for(int i = 1 ; i <= 26; i++){
+        char c = 'A' + (i - 1);
+        s ="";
+        s += c;
+        m.insert({s,i});
+    }
+   
+    int idx = 27;
+    s="";
+    for(int i = 0; i < msg.length();i++){
+        s.push_back(msg[i]);
+        if(m.find(s) == m.end()){
+            m.insert({s,idx});
+            idx++;
+            s.pop_back();
+            answer.push_back(m[s]);
+            s="";
+            i--;
+        }
+    }
+    answer.push_back(m[s]);
+    return answer;
+}
