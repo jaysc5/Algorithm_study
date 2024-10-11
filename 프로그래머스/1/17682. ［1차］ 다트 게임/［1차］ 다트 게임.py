214a8ad56ebe2ff684929a3082e67aafed6dc356
jaysc5 <113988349+jaysc5@users.noreply.github.com>
2024-10-12 00:20:14 +0900

    [level 1] Title: [1차] 다트 게임, Time: 3.08 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.py
+++ 프로그래머스/1/17682. ［1차］ 다트 게임/［1차］ 다트 게임.py
@@ -1,22 +1,22 @@
+import re
+
 def solution(dartResult):
-    stack = []
+    answer = []
+    pattern = re.compile(r'([0-9]|10)([SDT])([\*\#]?)')
+    cal = {
+        'S':lambda n:n,
+        'D':lambda n:n**2,
+        'T':lambda n:n**3,        
+    }
     
-    for idx, i in enumerate(dartResult):
-        if i == 'S':
-            pass
-        elif i == 'D':
-            stack[-1] **= 2
-        elif i == 'T':
-            stack[-1] **= 3
-        elif i == '*':
-            if (len(stack) >= 2):
-                stack[-2] *= 2
-            stack[-1] *= 2
-        elif i == '#':
-            stack[-1] *= -1
-        else:
-            if dartResult[idx:idx+2] == '10':
-                stack.append(10)
-            elif dartResult[idx-1:idx+1] != '10':
-                stack.append(int(i))
-    return sum(stack)
+    for n, b, o in pattern.findall(dartResult):
+        answer.append((cal[b](int(n))))
+        
+        if (o == '*'):
+            answer[-1] *= 2
+            if len(answer) >= 2:
+                answer[-2] *= 2
+        elif (o == '#'):
+            answer[-1] *= -1
+    
+    return sum(answer)
