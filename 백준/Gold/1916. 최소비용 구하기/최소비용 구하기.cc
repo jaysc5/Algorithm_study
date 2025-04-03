bd8e951add55c18e1dd177213d4769b7ac0db7f8
jaysc5 <113988349+jaysc5@users.noreply.github.com>
2025-04-03 17:09:40 +0900

    [Gold V] Title: 최소비용 구하기, Time: 96 ms, Memory: 4628 KB -BaekjoonHub

---
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.cc
@@ -0,0 +1,65 @@
+#include<iostream>
+#include<queue>
+#include<vector>
+#include<limits>
+
+using namespace std;
+
+struct Edge {
+	int num;
+	int cost;
+	bool operator<(Edge right)const {
+		if (cost < right.cost) return false;
+		if (cost > right.cost) return true;
+		return false;
+	}
+};
+
+int N, M;
+vector<Edge> alis[1001];
+int dist[1001];
+int INF = numeric_limits<int>::max();
+
+void func(int start) {
+	priority_queue<Edge> pq;
+	pq.push({ start, 0 });
+
+	for (int i = 0;i <= N;i++) dist[i] = INF;
+	dist[start] = 0;
+
+	while (!pq.empty()) {
+		Edge now = pq.top();
+		pq.pop();
+
+		if (dist[now.num] < now.cost) continue;
+
+		for (int i = 0;i < alis[now.num].size();i++) {
+			Edge next = alis[now.num][i];
+			int nextcost = dist[now.num] + next.cost;
+
+			if (nextcost >= dist[next.num]) continue;
+
+			dist[next.num] = nextcost;
+			pq.push({ next.num, nextcost });
+		}
+	}
+}
+
+int main() {
+	cin >> N >> M;
+
+	for (int i = 0;i < M;i++) {
+		int u, v, w;
+		cin >> u >> v >> w;
+		alis[u].push_back({ v, w });
+	}
+
+	int s, e;
+	cin >> s >> e;
+	
+	func(s);
+
+	cout << dist[e];
+
+	return 0;
+}
