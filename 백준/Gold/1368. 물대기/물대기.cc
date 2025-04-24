c438327711daa5e103d8416c50dab02e31985b7a
jaysc5 <113988349+jaysc5@users.noreply.github.com>
2025-04-24 23:00:53 +0900

    [Gold II] Title: 물대기, Time: 36 ms, Memory: 4340 KB -BaekjoonHub

---
+++ 백준/Gold/1368. 물대기/물대기.cc
@@ -0,0 +1,73 @@
+#include<iostream>
+#include<algorithm>
+#include<vector>
+
+using namespace std;
+
+struct Edge {
+	int A;
+	int B;
+	int cost;
+};
+
+bool cmp(const Edge& a, const Edge& b) {
+	return a.cost < b.cost;
+}
+
+int N;
+vector<Edge> edges;
+int parent[301];
+
+int Find(int now) {
+
+	if (now == parent[now])return now;
+	return parent[now] = Find(parent[now]);
+}
+
+void Union(int A, int B) {
+
+	int rootA = Find(A);
+	int rootB = Find(B);
+
+	if (rootA == rootB)return;
+	parent[rootB] = rootA;
+}
+
+int main() {
+
+	cin >> N;
+
+	for (int i = 1; i <= N; i++) {
+		int cost;
+		cin >> cost;
+		edges.push_back({ 0,i,cost });
+	}
+
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= N; j++) {
+			int cost;
+			cin >> cost;
+
+			if (i == j) continue;
+			edges.push_back({ i, j, cost });
+		}
+	}
+
+	sort(edges.begin(), edges.end(), cmp);
+
+	for (int i = 0; i <= N; i++) {
+		parent[i] = i;
+	}
+	
+	int sum = 0;
+	for (int i = 0; i < edges.size(); i++) {
+		Edge now = edges[i];
+
+		if (Find(now.A) == Find(now.B)) continue;
+		Union(now.A, now.B);
+		sum += now.cost;
+	}
+
+	cout << sum << "\n";
+
+}
