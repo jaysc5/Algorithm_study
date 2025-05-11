6000971e8d1d46d1973933f48dbfcbd40ce94313
jaysc5 <113988349+jaysc5@users.noreply.github.com>
2025-05-11 16:44:12 +0900

    [Silver I] Title: 점프, Time: 0 ms, Memory: 2148 KB -BaekjoonHub

--- 백준/Silver/1890. 점프/점프.cc
+++ 백준/Silver/1890. 점프/점프.cc
@@ -1,28 +1,20 @@
-#include<iostream>
+#include <iostream>
 #include<algorithm>
-#include<vector>
-#include<math.h>
 
 using namespace std;
 
 int N;
-int** arr;
-long long** dp;
+int arr[105][105];
+long long dp[105][105];
 
-int main() {
+int main()
+{
 	ios::sync_with_stdio(false);
 	cout.tie(NULL);
 	cin.tie(NULL);
 
 	cin >> N;
-
-	arr = new int* [N];
-	dp = new long long* [N];
-	for (int i = 0; i < N; i++) {
-		arr[i] = new int[N];
-		dp[i] = new long long[N] {0};
-	}
-
+	
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < N; j++) {
 			cin >> arr[i][j];
@@ -30,20 +22,18 @@ int main() {
 	}
 
 	dp[0][0] = 1;
-
+	
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < N; j++) {
 			if (dp[i][j] == 0 || arr[i][j] == 0) continue;
 
-			int jump = arr[i][j];
+			int k = arr[i][j];
 
-			if (i + jump < N) dp[i + jump][j] += dp[i][j];
-
-			if (j + jump < N) dp[i][j + jump] += dp[i][j];
+			if ((i + k) < N) dp[i + k][j] += dp[i][j];
+			if ((j + k) < N) dp[i][j + k] += dp[i][j];
 
 		}
 	}
-	
 
 	cout << dp[N - 1][N - 1];
 
