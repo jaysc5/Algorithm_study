2a0d528ef4b7ad884ae896640c47d97d8e169d72
jaysc5 <113988349+jaysc5@users.noreply.github.com>
2025-05-12 16:52:37 +0900

    [Silver V] Title: 거스름돈, Time: 0 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Silver/14916. 거스름돈/거스름돈.cc
+++ 백준/Silver/14916. 거스름돈/거스름돈.cc
@@ -10,20 +10,16 @@ int dp[100001];
 int main() {
 	cin >> N;
 
-	memset(dp, 999999, (N+1) * sizeof(int));
-	dp[0] = dp[1] = dp[3] = -1;
+	dp[0] = dp[1] = dp[3] = 999999;
 	dp[2] = dp[5] = 1;
 	dp[4] = 2;
 
 	for(int i=6;i<=N;i++){
-		int num2 = 999999, num5 = 999999;
-		if (dp[i - 2] != -1) num2 = dp[i - 2];
-		if (dp[i - 5] != -1) num5 = dp[i - 5];
-
-		dp[i] = min(dp[i], min(num2 + 1, num5 + 1));
+		dp[i] = min(dp[i - 2] + 1, dp[i - 5] + 1);
 	}
 
-	cout << dp[N];
+	if (dp[N] == 999999) cout << -1;
+	else cout << dp[N];
 
 	return 0;
 }
